ad11e82a9d53391426a6555e7bf427ca17324415
Ivan Law <ivan.law.199%5@gmail.com>
2015-09-05 17:01:37 -0700

    Initial commit of new website

---
+++ js/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+  $(".button-collapse").sideNav();
+  $('.scroll-link').on('click', function(event){
+    event.preventDefault();
+    var sectionID = $(this).attr("data-id");
+    console.log(sectionID);
+    scrollToID('#' + sectionID, 750);
+  });
+});
+
+function change(){
+  if(document.body.scrollTop != 0){
+    document.getElementById('nav-bar').classList.remove('unscrolled');
+  }
+  if(document.body.scrollTop == 0){
+    document.getElementById('nav-bar').classList.add('unscrolled');
+  }
+  // console.log('changed');
+};
+
+function scrollToID(id, speed){
+  var offSet = 50;
+  var targetOffset = $(id).offset().top - offSet;
+  var mainNav = $('#main-nav');
+  $('html,body').animate({scrollTop:targetOffset}, speed);
+}
+
+var imgList = document.getElementsByClassName("raining-img");
+function removeClass(){
+  for(i=0; i<imgList.length; i++)
+    imgList[i].style.visibility = "visible";
+}
