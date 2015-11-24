1c07c8371845f3eba6bbeae252e40d6d96c7eb7c
KEvin Wang <kevinwang@kevinwang.us>
2015-11-23 22:40:17 -0800

    fixed having no closing tag for <a> element

--- js/script.js
+++ js/script.js
@@ -13,10 +13,10 @@ $(document).ready(function(){
 });
 
 function getTimeRemaining(endtime){
-  var t = Date.parse(endtime) - Date.parse(new Date());
+  var t = /*Date.parse(endtime) -*/ Date.parse(new Date());
   var seconds = Math.floor( (t/1000) % 60 );
   var minutes = Math.floor( (t/1000/60) % 60 );
-  var hours = Math.floor( (t/(1000*60*60)) % 24 );
+  var hours = Math.floor( (t/(1000*60*60)) % 24 ) - 8;
   var days = Math.floor( t/(1000*60*60*24) );
   return {
     'total': t,
