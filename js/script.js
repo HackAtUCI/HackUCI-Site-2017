73fb23d9ce98018e96b1c0689f9f9751c18c7e61
KEvin Wang <kevinwang@kevinwang.us>
2015-11-24 01:59:09 -0800

    made clock go away :clock1:

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
 });
 
 function getTimeRemaining(endtime){
-  var t = /*Date.parse(endtime) -*/ Date.parse(new Date());
+  var t = Date.parse(endtime) - Date.parse(new Date());
   var seconds = Math.floor( (t/1000) % 60 );
   var minutes = Math.floor( (t/1000/60) % 60 );
   var hours = Math.floor( (t/(1000*60*60)) % 24 ) - 8;
